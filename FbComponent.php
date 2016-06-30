b2330c2c73c55249966021175d4cb78830a77ac0
alexeydibrovalight <AlexeyDibrovaLight@gmail.com>
2016-06-30 16:36:18 +0300

    access token init component

--- FbComponent.php
+++ FbComponent.php
@@ -9,8 +9,12 @@ use Facebook\Helpers\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\GraphNodes\GraphUser;
 use Exception;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 
-class Facebook extends Component{
+
+class FbComponent extends Component{
 
     public $appId;
 
@@ -22,22 +26,85 @@ class Facebook extends Component{
 
     private $accessToken;
 
-    public function init()
-    {
+    public function init(){
+
         parent::init();
 
-        if (!Yii::$app->session->isActive) {
-            Yii::$app->session->open();
-        }
-        $client  = new FacebookApp($this->appId, $this->secret);
+        $fb_client = new Facebook([
+            'app_id' => $this->appId,
+            'app_secret' => $this->secret,
+            'default_graph_version' => 'v2.2',
+        ]);
 
+        $fb_helper = $fb_client->getRedirectLoginHelper();
 
-        $accessToken = Yii::$app->session->get('fbAccessToken');
-        if ($accessToken !== null) {
-            $client->getAccessToken();
-            $this->setSession($client);
+        try {
+            $accessToken = $fb_helper->getAccessToken();
+
+        } catch(FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
         }
+
+        if (! isset($accessToken)) {
+            if ($fb_helper->getError()) {
+                header('HTTP/1.0 401 Unauthorized');
+                echo "Error: " . $fb_helper->getError() . "\n";
+                echo "Error Code: " . $fb_helper->getErrorCode() . "\n";
+                echo "Error Reason: " . $fb_helper->getErrorReason() . "\n";
+                echo "Error Description: " . $fb_helper->getErrorDescription() . "\n";
+            } else {
+                header('HTTP/1.0 400 Bad Request');
+                echo 'Bad request';
+            }
+            exit;
+        }
+
+        echo '<h3>Access Token</h3>';
+        var_dump($accessToken->getValue());
+
+        $oAuth2Client = $fb_client->getOAuth2Client();
+        $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+        echo '<h3>Metadata</h3>';
+        var_dump($tokenMetadata);
+        $tokenMetadata->validateAppId($this->appId);
+        $tokenMetadata->validateExpiration();
+
+        if (! $accessToken->isLongLived()) {
+            // Exchanges a short-lived access token for a long-lived one
+            try {
+                $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+            } catch (FacebookSDKException $e) {
+                echo "<p>Error getting long-lived access token: " . $fb_helper->getMessage() . "</p>\n\n";
+                exit;
+            }
+
+            echo '<h3>Long-lived</h3>';
+            var_dump($accessToken->getValue());
+        }
+
+        $_SESSION['fb_access_token'] = (string) $accessToken;
+
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $fb_client->get('/me?fields=id,name', $accessToken);
+        } catch(FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+        $user = $response->getGraphUser();
+
     }
+
 
     public function getSession()
     {
